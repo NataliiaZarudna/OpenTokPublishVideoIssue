6837e03878d735609bc7fdfcc21dd980b4d5a351
Nataliia <natalija.sirobaba@gmail.com>
2020-09-08 15:25:41 +0300

    Upgrade OpenTok version

--- app/src/main/java/com/tokbox/android/tutorials/basic_video_chat/MainActivity.java
+++ app/src/main/java/com/tokbox/android/tutorials/basic_video_chat/MainActivity.java
@@ -1,33 +1,12 @@
 package com.tokbox.android.tutorials.basic_video_chat;
 
 import android.content.Intent;
-import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
-import android.Manifest;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.widget.Toast;
 
-import com.opentok.android.Session;
-import com.opentok.android.Stream;
-import com.opentok.android.Publisher;
-import com.opentok.android.PublisherKit;
-import com.opentok.android.Subscriber;
-import com.opentok.android.BaseVideoRenderer;
-import com.opentok.android.OpentokError;
-import com.opentok.android.SubscriberKit;
 import com.tokbox.android.tutorials.basicvideochat.R;
-
-import java.util.List;
-
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.AppSettingsDialog;
-import pub.devrel.easypermissions.EasyPermissions;
 
 
 public class MainActivity extends AppCompatActivity {
